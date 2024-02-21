34b4f8c6f73cdd2c11a3f3c631c5b88633c0727b
rishigirivenkateshias <160624307+rishigirivenkateshias@users.noreply.github.com>
2024-02-21 08:00:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: [  "Speed learner","Future Enterprenuer", "Gym player"],
+    TypeSpeed:100,
+    backSpeed:101,
+    backDelay:1000,
+    loop:true
+})
